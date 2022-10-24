38315a15bfa7f755ba2f87100c8dc9c9fab081fc
Manny <manuelpo.msn.com>
2022-10-23 21:39:56 -0600

    add request and render to browser

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=taylor%20swift&pageNumber=1&pageSize=10&autoCorrect=true",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "",
-		"X-RapidAPI-Host": "contextualwebsearch-websearch-v1.p.rapidapi.com"
-	}
-};
+const filteredBySearch = (searching) => {
+  const URL =
+    "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI";
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+  const API_KEY = ``
+  //"";
+  const QUERY = `?q=${searching}`;
+  const PAGE_NUMBER = `&pageNumber=${1}`;
+  const PAGE_SIZE = `&pageSize=${10}`;
+
+  const OPTIONS = `&autoCorrect=true&fromPublishedDate=null&toPublishedDate=null`;
+
+  const settings = {
+    async: true,
+    crossDomain: true,
+    url: URL + QUERY + PAGE_NUMBER + PAGE_SIZE + OPTIONS,
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": API_KEY,
+      "X-RapidAPI-Host": "contextualwebsearch-websearch-v1.p.rapidapi.com",
+    },
+  };
+
+  const $main = $("main");
+  //filter () =>
+  const request = $.ajax(settings);
+  const response = request
+    .then((response) => {
+      const objData = response;
+      objData.value.map((element) => {
+        //first div in main
+        const $div1 = $("<div>");
+        $div1.html(`<p>${element.title}</p>`);
+        $main.append($div1);
+
+        //second div in main
+        const $div2 = $("<div>");
+        $div2.html(`<p>${element.url}</p>`);
+        $main.append($div2);
+
+        // // third div in main
+        const $div3 = $("<div>");
+        $div3.html(`<image src=${element.image.url}>`);
+        $main.append($div3);
+      });
+      console.log(response);
+    })
+
+    .catch((err) => console.log("ERROR =====>", err.responseJSON.message));
+};
+//filteredBySearch('taylor swift');
